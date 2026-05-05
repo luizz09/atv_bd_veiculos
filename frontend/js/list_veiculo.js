7324a49c8f3842d8740735bfca8d9c6308a77b91
Luiz <patinhozzvluiz@gmail.com>
2026-05-05 15:23:16 -0300

    terceira versao

---
+++ frontend/js/list_veiculo.js
@@ -0,0 +1,19 @@
+fetch('http://localhost:3000/veiculos')
+.then(res => res.json())
+.then(veiculos => {
+    const corpo = document.getElementById('corpo')
+    veiculos.forEach(v => {
+        corpo.innerHTML += `
+            <tr>
+                <td>${v.codVeiculo}</td>
+                <td>${v.nome}</td>
+                <td>${v.marca}</td>
+                <td>${v.categoria}</td>
+                <td>${v.ano}</td>
+                <td>${v.precoFabrica}</td>
+                <td>${v.precoVenda}</td>
+            </tr>
+        `
+    })
+})
+.catch(err => console.error('Erro ao listar veículos', err))
