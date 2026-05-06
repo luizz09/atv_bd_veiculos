947ee07a98d39d770db6def36c92da43dd128182
LUIZ HENRIQUE DE ASSIS SAMPAIO <luiz_h_sampaio@estudante.sesisenai.org.br>
2026-05-06 16:08:07 -0300

    quinta versao

--- frontend/js/list_veiculo.js
+++ frontend/js/list_veiculo.js
@@ -5,12 +5,12 @@ fetch('http://localhost:3000/veiculos')
     veiculos.forEach(v => {
         corpo.innerHTML += `
             <tr>
-                <td>${v.codVeiculo}</td> |
-                <td>${v.nome}</td> |
-                <td>${v.marca}</td> |
-                <td>${v.categoria}</td> |
-                <td>${v.ano}</td> |
-                <td>${v.precoFabrica}</td> |
+                <td>${v.codVeiculo}</td>
+                <td>${v.nome}</td>
+                <td>${v.marca}</td>
+                <td>${v.categoria}</td>
+                <td>${v.ano}</td>
+                <td>${v.precoFabrica}</td>
                 <td>${v.precoVenda}</td>
             </tr>
         `
